7d7491a08562a9fef741cbfec56bfd765638ba20
Hélio Conde <68510529+HelioConde@users.noreply.github.com>
2023-10-23 23:50:05 -0300

    Update apiConfig.jsx

--- src/pages/apiConfig.jsx
+++ src/pages/apiConfig.jsx
@@ -2,10 +2,10 @@
 const API_URL = 'https://gracious-elgamal.173-249-10-142.plesk.page/api';
 
 // Define o token de autorização da API, que é usado para autenticar solicitações à API
-const apiAuthorizationToken = 'Bearer $2y$10$x3wqNWc4ZonF6dVWKAPnMuU1A258mgKbGWziVPdL5mhzqQwlhQEqK';
+const apiAuthorizationToken = '';
 
 // Define o token do usuário, que pode ser usado para autenticar um usuário na aplicação
-const userToken = 'Bearer 35|bkdTKk4t5WoNeApCMkVwWLBhmkjtarmeULMfwKiW';
+const userToken = '';
 
 // Exporta as constantes para que possam ser importadas em outros arquivos
 export { API_URL, apiAuthorizationToken, userToken };
